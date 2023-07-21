3063ae55e8cad3bcfed592825b426a739453b68c
Rahul Sharma <rahulsharma841990@outlook.com>
2023-07-21 17:55:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/screenshot', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).send('URL parameter is missing.');
+  }
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const screenshot = await page.screenshot({ fullPage: true });
+    await browser.close();
+
+    res.set('Content-Type', 'image/png');
+    res.send(screenshot);
+  } catch (error) {
+    console.error('Error capturing screenshot:', error);
+    res.status(500).send('Error capturing screenshot.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
