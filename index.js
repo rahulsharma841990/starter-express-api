f2e4c2dcbfd61fc1dcae5481e4bc5282b3af6074
Rahul Sharma <rahulsharma841990@outlook.com>
2023-07-24 12:19:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 app.get('/screenshot', async (req, res) => {
   const { url } = req.query;
